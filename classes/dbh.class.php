b48cc40e1dd61226f1fedb12ac4f4401fbc6796f
MSI\jonas <r0840132@student.thomasmore.be>
2021-05-07 14:47:20 +0200

    Jonas upload ook

--- classes/dbh.class.php
+++ classes/dbh.class.php
@@ -7,6 +7,11 @@ class dbh {
     private $dbPassword = "SportBudG@ng2021";
     private $dbName = "ID329040_sportbud";
 
+    private $dbHostMessages = "ID329040_sportbuddata.db.webhosting.be";
+    private $dbUsernameMessages = "ID329040_sportbuddata";
+    private $dbPasswordMessages = "SportBudG@ng2021";
+    private $dbNameMessages = "ID329040_sportbuddata";
+
     #private $dbHost = "localhost:8889";
     #private $dbUsername = "root";
     #private $dbPassword = "root";
@@ -15,6 +20,14 @@ class dbh {
     protected function connect(){
         $dsn = 'mysql:host=' . $this->dbHost . ';dbname=' . $this->dbName;
         $pdo = new PDO($dsn, $this->dbUsername, $this->dbPassword);
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+        return $pdo;
+    }
+
+    protected function connectMessages(){
+        $dsn = 'mysql:host=' . $this->dbHostMessages . ';dbname=' . $this->dbNameMessages;
+        $pdo = new PDO($dsn, $this->dbUsernameMessages, $this->dbPasswordMessages);
         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
         return $pdo;
