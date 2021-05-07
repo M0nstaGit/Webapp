7634ae9197177b5268a8e16c3390176acf91c382
Jordi Timmers <timmersjordi@outlook.com>
2021-05-07 17:53:21 +0200

    Styling makeAccount page

--- makeAccount.php
+++ makeAccount.php
@@ -7,65 +7,56 @@
     <link rel="stylesheet" href="css/style.css">
     <title>Create Account</title>
 </head>
-    <body>
+    <body class="scrollable">
         <?php include "/includes/header.php"?>
+        <div class="centerTitle">
+            <h1>Create account</h1>
+        </div>
         <main class="form">
             <form class="formMargin" action="insert.php" method="POST">
                 <table class="formTable m-auto">
                     <tbody>
                         <tr>
-                            <td><label for="firstName">Fill in your first name:</label></td>
-                            <td><input type="text" id="firstName" name="firstName" placeholder="First Name" required/></td>
+                            <td><input type="text" class="createAccField" id="firstName" name="firstName" placeholder="First Name" required/></td>
                         </tr>
                         <tr>
-                            <td><label for="lastName">Fill in your last name:</label></td>
-                            <td><input type="text" id="lastName" name="lastName" placeholder="Last Name" required/></td>
+                            <td><input type="text" class="createAccField" id="lastName" name="lastName" placeholder="Last Name" required/></td>
                         </tr>
                         <tr>
-                            <td><label for="email">E-mail:</label></td>
-                            <td><input type="email" id="email" name="email" placeholder="E-mail" required/></td>
+                            <td><input type="email" class="createAccField" id="email" name="email" placeholder="E-mail" required/></td>
                         </tr>
                         <tr>
-                            <td><label for="phone">Phone number:</label></td>
-                            <td><input type="tel" id="phone" name="phone" placeholder="Phone number" required/></td>
+                            <td><input type="tel" class="createAccField" id="phone" name="phone" placeholder="Phone number" required/></td>
                         </tr>
                         <tr>
-                            <td><label for="birthdate">Birthdate:</label></td>
-                            <td><input type="date" id="birthdate" name="birthdate" required/></td>
+                            <td><input type="date" class="createAccField" id="birthdate" name="birthdate" required/></td>
                         </tr>
                         <tr>
-                            <td><label for="locationId">Your location</label></td>
-                            <td><input type="text" id="locationId" name="locationId" required/></td>
+                            <td><input type="text" class="createAccField" id="locationId" name="locationId" placeholder="Location" required/></td>
                         </tr>
                         <tr>
-                            <td><label for="description">Give us a description: </label></td>
-                            <td><input type="text" id="description" name="description" placeholder="Description" required /></td>
+                            <td><input type="text" class="createAccField" id="description" name="description" placeholder="Description" required /></td>
                         </tr>
                         <tr>
-                            <td><label for="pictureURL">Choose a profile picture:</label></td>
-                            <td><input type="file" id="pictureURL" name="pictureURL"/></td>
+                            <td><input type="file" class="createAccField" id="pictureURL" name="pictureURL"/></td>
                         </tr>
                         <tr>
-                            <td><label for="gender">Give us your gender:</label></td>
-                            <td><select id="gender" name="gender">
-                                <option value="0">Men</option>
-                                <option value="1">Woman</option>
+                            <td><select class="createAccField" id="gender" name="gender">
+                                <option value="0">Gender: Men</option>
+                                <option value="1">Gender: Woman</option>
                                 </select></td>
                         </tr>
                         <tr>
-                            <td><label for="preferredGender">Give us your preferred gender:</label></td>
-                            <td><select id="preferredGender" name="preferredGender" placeholder="Preffered gender" required>
-                                <option value="0">Men</option>
-                                <option value="1">Woman</option>
+                            <td><select class="createAccField" id="preferredGender" name="preferredGender" placeholder="Preffered gender" required>
+                                <option value="0">Preferred gender: Men</option>
+                                <option value="1">Preferred gender: Woman</option>
                                 </select></td>
                         </tr>
                         <tr>
-                            <td><label for="userName">Choose a username:</label></td>
-                            <td><input type="text" id="userName" name="userName" required/></td>
+                            <td><input type="text" id="userName" name="userName" class="createAccField" placeholder="Username" required/></td>
                         </tr>
                         <tr>
-                            <td><label for="password">Choose a password:</label></td>
-                            <td><input type="password" id="password" name="password" required/></td>
+                            <td><input type="password" id="password" name="password" class="createAccField" placeholder="Password" required/></td>
                         </tr>
                         <tr>
                             <td><input type="submit" value="Create my account!" class="myButton"/></td>
@@ -74,5 +65,6 @@
                 </table>
             </form>
         </main>
+        <img class="bottomscreen" src="./images/bot.png" alt="bottom">
     </body>
 </html>
