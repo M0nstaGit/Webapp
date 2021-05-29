63b85c857d2619c32d27603f6a9bd04f06f916bd
MSI\jonas <r0840132@student.thomasmore.be>
2021-05-29 22:14:48 +0200

    laatste versie messages

--- classes/messages.class.php
+++ classes/messages.class.php
@@ -5,51 +5,53 @@ class messages extends user {
 
     //GETS ALL ID'S FROM CURRENT USERS FRIENDS
     public function getFriends(){
-
         //GETS CURRENT USER
         $currentUserId = $this->getuserid();
 
-        $sql = "SELECT userId2 from friends WHERE userId1 = $currentUserId AND relationStatus = 1";
-
-        $stmt = $this->connect2()->prepare($sql);
-
-        $ids = $stmt->fetchall();
+        $sqlFriends = "SELECT userId1,userId2 FROM friends WHERE userId1 = $currentUserId AND relationStatus = 1 OR userId2 = $currentUserId AND relationStatus = 1";
+        $stmtFriends = $this->connect2()->prepare($sqlFriends);
+        $stmtFriends->execute();
+        $ids = $stmtFriends->fetchall();
         return $ids;
     }
 
     //GETS ALL THE NAMES OF THE IDS THE GETFRIENDS FUNCTION FILTERED OUT
-    public function getNames($friendId){
-        $sql = "SELECT firstName,lastName from user WHERE userId = ?";
+    public function getNames(){
+        //GETS CURRENT USER
+        $currentUserId = $this->getuserid();
 
-        $stmt = $this->connect2()->prepare($sql);
+        $sqlUserId = "SELECT firstName,lastName FROM user WHERE userId = (SELECT userId1 FROM friends WHERE userId1 = $currentUserId AND relationStatus = 1 OR userId2 = $currentUserId AND relationStatus = 1)";
 
-        $stmt->execute([$friendId]);
+        $stmtUserId = $this->connect2()->prepare($sqlUserId);
 
-        $friendNames = $stmt->fetchall();
-        return $names;
+        $stmtUserId->execute();
+
+        $friendNames = $stmtUserId->fetchall();
+        return $friendNames;
     }
 
     //GETS ALL THE MESSAGES OF THE CURRENTUSER AND THE SELECTED FRIEND
     public function getMessages($userId1,$userId2){
-        $sql = "SELECT userid1,userid2,`message` FROM `message` WHERE userId2 = ? AND userId1 = ? OR userId1 = ? AND userId2 = ?";
+        
+        $sqlMessages = "SELECT userid1,userid2,`message` FROM `message` WHERE userId2 = ? AND userId1 = ? OR userId1 = ? AND userId2 = ?";
 
-        $stmt = $this->connect2()->prepare($sql);
+        $stmtMessages = $this->connect2()->prepare($sqlMessages);
 
-        $stmt->execute([$user1,$user2,$user1,$user2]);
+        $stmtMessages->execute([$user1,$user2,$user1,$user2]);
 
-        $messages = $stmt->fetchall();
+        $messages = $stmtMessages->fetchall();
         //WE ONLY NEED USERID1 BECAUSE THE PREVIOUS SQL STATEMENT ONLY FILTERED OUT THE MESSAGES THE USER AND THE SELECTED FRIEND SEND TO EACHOTHER
         //AND USERID1 IS THE USER WHO SENT THE MESSAGE
-        return $messages['userid1','message'];
+        return $messages;
     }
 
     //SETS THE TYPED MESSAGE TO THE DATABASE
     public function setMessage($userId1,$userId2,$messageInput){
-        $sql = "INSERT INTO `message`(userid1,userid2,`message`) VALUES (?,?,?)";
+        $sqlsetMessage = "INSERT INTO `message`(userid1,userid2,`message`) VALUES (?,?,?)";
 
-        $stmt = $this->connect2()->prepare($sql);
+        $stmtsetMessage = $this->connect2()->prepare($sqlsetMessage);
 
-        $stmt->execute([$userId1,$userId2,$messageInput]);
+        $stmtsetMessage->execute([$userId1,$userId2,$messageInput]);
     }
 
 
--- messages.php
+++ messages.php
@@ -1,18 +1,46 @@
-<!--test id's zijn id1:26  id2:30-->
+<!--test id's zijn id1:1  id2:54-->
 <?php
+    // Report all errors except E_NOTICE   
+    error_reporting(E_ALL ^ E_NOTICE);
+
     session_start();
     include 'includes/class-autoload.inc.php';
     if(empty($_SESSION['username'])){
         header("Location: login.php");
     }
-    $currentUserId = new user()
+
+    function object_to_array($data)
+    {
+        if(is_array($data) || is_object($data))
+        {
+            $result = array();
+        
+            foreach($data as $key => $value) {
+                $result[$key] = $this->object_to_array($value);
+            }
+        
+            return $result;
+        }
+    
+        return $data;
+    }
+
+    $currentUserId = new user();
     $currentUserId->getuserid();
 
     $friendIds = new messages();
     $friendIds->getFriends();
+    $friendIdsArray = object_to_array($friendIds);
+    var_dump( (array) $friendIdsArray);
 
     $friendNames = new messages();
-    $friendNames->getNames($friendIds)
+    $friendNames->getNames($friendIds);
+    $friendNamesArray = object_to_array($friendNames);
+    
+    var_dump( (array) $friendNamesArray );
+
+
+    
 ?>
 <!-- Messages page -->
 
@@ -27,10 +55,13 @@
         <link rel="stylesheet" href="./css/loadanimation.css">
         <link rel="shortcut icon" type="image/png" href="./images/sportbud.png">
     </head>
-
+    
     <body>
         <?php include_once 'includes/header.php'?>
         
+        <input type="hidden" name="friendNamesArray" value="<?php echo $friendNamesArray; ?>" id="friendNamesArray">
+        <input type="hidden" name="friendIdsArray" value="<?php echo $friendIdsArray; ?>" id="friendIdsArray">
+
         <div class="messagePreview" id="messagePreview">
             
         </div>
@@ -41,15 +72,32 @@
 
 
         <script>
-            var friends,flen,text,i;
+            var friends = document.querySelector('friendNamesArray')
+            var flen,text,i;
 
-            friends = "<?php echo $friendNames; ?>";
+            //friends = ["jonas","vantrappen",
+            //           "louis","debacker",
+            //           "jordi","timmers",]
+
+            console.log(friends);
+
             fLen = friends.length;
             
             text = '<div class="friendDisplay">';
-            for (i = 0; i < fLen; i++) {
-            
-                text += '<a href="messagesPersonal.php" id="'i'" class="friend">' + friends[i] + '</a>';
+            for (i = 0; i < fLen; i=i+2) {
+                console.log("we zitten in de FOR1");
+                var j = 0;
+                text += '<a href="messagesPersonal.php" id="'+ j +'" class="friend">';
+                for (k = 0; k < flen; k=k+1){
+                    console.log("we zitten in de FOR2");
+                    if(k==null){
+                        console.log("we zitten in de if == null");
+                        break
+                    }
+                    text += friends[i][k]
+                }
+                text += '</a>';
+                j++;
             }
             text += "</div>";
 
